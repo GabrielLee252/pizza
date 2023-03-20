e830039646f47417c0ee9a97ceba09f52f8c3a2d
Gabe Lee <gibgab6@gmail.com>
2023-03-20 18:48:10 -0400

    finished the second one

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ function pizzaOven(crustType, sauceType, cheeses, toppings) {
   const myPizza = pizzaOven("deep dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"]);
   console.log(myPizza);
   
+  function pizzaOven(crustType, sauceType, cheeses, toppings) {
+    let pizza = {
+      crustType: crustType,
+      sauceType: sauceType,
+      cheeses: cheeses,
+      toppings: toppings
+    };
+    return pizza;
+  }
+  
+  const myPizza2 = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
+  console.log(myPizza);
